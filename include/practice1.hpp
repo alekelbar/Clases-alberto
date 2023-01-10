5231d86c2af4721665f3569ba8eb41a706baa642
alekelbar <alekelbar.personal@gmail.com>
2023-01-10 12:13:38 -0600

    preparate 1

---
+++ include/practice1.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+
+// ! EJERCICIOS DE ESTA TUTORÍA
+// En una tienda se ofrece un descuento del 15% sobre el total de la compra y
+// un cliente desea saber cuánto deberá pagar finalmente por su compra.
+
+// Realice una función que calcule su pago recibiendo su coste.
+double cost(double totalCost);
+
+// Un vendedor recibe un sueldo base más un 10 % extra por comisión de sus ventas.
+// El vendedor desea saber cuánto dinero obtendrá por concepto de comisiones
+// por las tres ventas que realiza en el mes y el total que recibirá en el mes
+// tomando en cuenta su sueldo base y sus comisiones.
+
+// Realice una función que calcule su comición recibiendo su la comisión.
+double salary(double comitions);
